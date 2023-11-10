14018609418c3d36f12cc15110da0592038e64ac
douglasgaspar <douglasgaspar@gmail.com>
2023-11-10 21:03:22 +0000

    Fri, Nov 10, 2023, 6:03 PM -03:00

--- pset4/credit/credit.c
+++ pset4/credit/credit.c
@@ -3,27 +3,26 @@
 #include <stdlib.h>
 
 int main(void){
-    char numCartao[16];
-    printf("Informe o número do cartão: ");
-    gets(numCartao);
+    long numCartao = 0;
 
-    int qtdeDigitos = strlen(numCartao);
-    while(qtdeDigitos < 13 || qtdeDigitos > 16){
-        printf("Valor inválido, informe novamente: ");
-        scanf("%s", numCartao);
-        qtdeDigitos = strlen(numCartao);
+    do{
+        printf("Informe o número do cartão: ");
+        numCartao = get_long();
+    }while(numCartao <= 0);
+
+    int qtDigitos = 0;
+    long copiaCartao = numCartao;
+
+    while(copiaCartao > 0){
+        copiaCartao /= 10;
+        qtDigitos++;
     }
+
+    printf("%d ", qtDigitos);
 
     //34 37 Amex
     //51, 52, 53, 54 ou 55 Master
     //Visa inicia 4
-
-    int somaDobro = 0;
-    int cadaValor = 0;
-    for(int i=qtdeDigitos-2 ; i>=0; i=i-2){
-        printf("%c ", cadaValor[i]);
-    }
-
 
     printf("\n%d", qtdeDigitos);
 
