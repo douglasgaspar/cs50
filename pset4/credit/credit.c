6f7a0ac07fda56f83f10a1731e48e6ac18ead957
douglasgaspar <douglasgaspar@gmail.com>
2023-11-10 17:26:31 +0000

    Fri, Nov 10, 2023, 2:26 PM -03:00

--- pset4/credit/credit.c
+++ pset4/credit/credit.c
@@ -3,8 +3,8 @@
 #include <math.h>
 
 int main(void){
-    float troco = get_float("Qual o valor do troco?");
-    while(troco <= 0){
+    long numCartao = get_long("Informe o número do cartão: ");
+    while(numCartao <= 0){
         printf("Valor do troco inválido. Digite novamente.\n");
         troco = get_float("Qual o valor do troco?");
     }
