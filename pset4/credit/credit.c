e1cae05a96ef82513aeb0e2809363e3a48b89067
douglasgaspar <douglasgaspar@gmail.com>
2023-11-10 17:08:21 +0000

    Fri, Nov 10, 2023, 2:08 PM -03:00

---
+++ pset4/credit/credit.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+    float troco = get_float("Qual o valor do troco?");
+    while(troco <= 0){
+        printf("Valor do troco inválido. Digite novamente.\n");
+        troco = get_float("Qual o valor do troco?");
+    }
+}
