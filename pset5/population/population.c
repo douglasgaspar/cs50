411a0ed09847cc7f86cbbce04065be0ea13db955
douglasgaspar <douglasgaspar@gmail.com>
2023-11-11 18:30:21 +0000

    Sat, Nov 11, 2023, 3:30 PM -03:00

---
+++ pset5/population/population.c
@@ -0,0 +1,14 @@
+#include
+#include
+
+int main(void)
+{
+      // TODO: Solicite o valor inicial ao usuário
+
+     // TODO: Solicite o valor final ao usuário
+
+     // TODO: Calcule o número de anos até o limite
+
+     // TODO: Imprima o número de anos
+
+}
