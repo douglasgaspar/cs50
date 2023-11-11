5315568ed07aa1a059b3944aef9679cfdc36c0f4
douglasgaspar <douglasgaspar@gmail.com>
2023-11-11 18:33:10 +0000

    Sat, Nov 11, 2023, 3:33 PM -03:00

--- pset5/population/population.c
+++ pset5/population/population.c
@@ -1,8 +1,23 @@
-#include
-#include
+#include<cs50.h>
+#include<stdio.h>
 
 int main(void)
 {
+    int numeroInicial = 0;
+    int numeroFinal = 0;
+    do
+    {
+        numeroInicial = get_int("Número inicial de lhamas: ");
+        printf("\n");
+    }while (numeroInicial < 9);
+
+    do
+    {
+        numeroFinal = get_int("Número final de lhamas: ");
+        printf("\n");
+    }
+    numeroInicial = get_int("Número final de lhamas: ");
+    while (numeroFinal < 9);
       // TODO: Solicite o valor inicial ao usuário
 
      // TODO: Solicite o valor final ao usuário
