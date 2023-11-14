8871224759447240cf26d0db42e5ab5e1a215af2
douglasgaspar <douglasgaspar@gmail.com>
2023-11-14 23:06:57 +0000

    Tue, Nov 14, 2023, 8:06 PM -03:00

--- pset6/readability/readability.c
+++ pset6/readability/readability.c
@@ -5,6 +5,8 @@
 int main(void){
     string frase = get_string("Text: ");
 
+    char delimitadores[] = "!?.";
+
     for (int i = 0 ; i < strlen(frase) ; i++){
         if (islower(s[i])){
             printf("%c", toupper(s[i]));
