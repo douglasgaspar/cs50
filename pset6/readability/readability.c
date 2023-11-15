0b0b2bff952aab278e9ad8e0768dc125debec4ec
douglasgaspar <douglasgaspar@gmail.com>
2023-11-15 00:11:35 +0000

    Tue, Nov 14, 2023, 9:11 PM -03:00

--- pset6/readability/readability.c
+++ pset6/readability/readability.c
@@ -14,17 +14,21 @@ int main(void){
     //índice = 0,0588 * L - 0,296 * S - 15,8
     //L contar as letras e dividir pela quantidade de palavras. Multiplicar por 100
     //L é o número médio de letras por 100 palavras
+    //S quantidade de frases / quantidade de palavras. Multiplica por 100
     //S é o número médio de sentenças por 100 palavras no texto.
 
+    float S = (frases / palavras) * 100;
+    printf("%f", S);
 
 
-    for (int i = 0 ; i < strlen(frase) ; i++){
-        if (islower(s[i])){
-            printf("%c", toupper(s[i]));
-        }else{
-            printf("%c", s[i]);
-        }
-    }
+
+    // for (int i = 0 ; i < strlen(frase) ; i++){
+    //     if (islower(s[i])){
+    //         printf("%c", toupper(s[i]));
+    //     }else{
+    //         printf("%c", s[i]);
+    //     }
+    // }
     printf("\n");
 
 }
