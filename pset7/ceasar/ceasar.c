406bb1039bd3b762a518fc61cce216f080fc9b0c
douglasgaspar <douglasgaspar@gmail.com>
2023-11-17 17:43:57 +0000

    Fri, Nov 17, 2023, 2:43 PM -03:00

--- pset7/ceasar/ceasar.c
+++ pset7/ceasar/ceasar.c
@@ -23,9 +23,16 @@ int main(int argc, string argv[]){
                 strchr(argv[1], '3') != NULL && strchr(argv[1], '4') != NULL && strchr(argv[1], '5') != NULL &&
                     strchr(argv[1], '6') != NULL && strchr(argv[1], '7') != NULL && strchr(argv[1], '8') != NULL &&
                         strchr(argv[1], '9') != NULL){
+                string chave = argv[1];
                 for(int i=0 ; i<strlen(argv[1]); i++){
-                    
+                    if(strchr(argv[1], chave[i])){
+                        printf("Caractere duplicado na chave");
+                        break;
+                        return 1;
+                    }
                 }
+
+                printf("OK");
             }else{
                 printf("A chave não deve possuir números");
                 return 1;
