dca9247f30d7279c0efb62263a3aaf9df4429a3e
douglasgaspar <douglasgaspar@gmail.com>
2023-11-21 20:25:26 +0000

    Tue, Nov 21, 2023, 5:25 PM -03:00

--- pset7/caesar/caesar.c
+++ pset7/caesar/caesar.c
@@ -14,15 +14,10 @@ int main(int argc, string argv[]){
         if(isdigit(char1) != 0){ //Verificar se é número
             string textoOriginal = get_string("plaintext: ");
             int chave = atoi(argv[1]);
-            int chaveCopia = chave;
-            //printf("CHAVE: %i", chave);
 
             printf("ciphertext: ");
 
             for(int i=0 ; i<(int)strlen(textoOriginal) ; i++){
-                //printf(">>> %i", (int)textoOriginal[i] + chave);
-                chave = chaveCopia;
-
                 if(isalpha(textoOriginal[i]) != 0){
                     if(islower(textoOriginal[i])){
                         printf("%c", (textoOriginal[i] - 97 + chave) % 26 + 97);
@@ -31,26 +26,8 @@ int main(int argc, string argv[]){
                     }else{
                         printf("%c", textoOriginal[i]);
                     }
-
-                    //     if(((int)textoOriginal[i] + chave) > 122){
-                    //         chave = ((int)textoOriginal[i] + chave) - 122;
-                    //         printf("%c", 96 + chave);
-                    //     }else{
-                    //         printf("%c", tolower(textoOriginal[i] + chave));
-                    //     }
-                    // }else{
-                    //     if(((int)textoOriginal[i] + chave) > 90){
-                    //         chave = ((int)textoOriginal[i] + chave) - 90;
-                    //         printf("%c", 64 + chave);
-                    //     }else{
-                    //         printf("%c", toupper(textoOriginal[i] + chave));
-                    //     }
-                    // }
-                //}else{
-                //    printf("%c", textoOriginal[i]);
                 }
             }
-            printf("\n");
         }else{
             printf("Usage: ./caesar key");
             return 1;
