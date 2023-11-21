6dd0982bd388d9c89fc54767210900b3863d5d23
douglasgaspar <douglasgaspar@gmail.com>
2023-11-21 22:31:44 +0000

    Tue, Nov 21, 2023, 7:31 PM -03:00

--- pset9/scrabble/scrabble.c
+++ pset9/scrabble/scrabble.c
@@ -21,7 +21,7 @@ int main(void){
     if(score1 > score2){
         printf("Player 1 wins!\n");
     }else if(score2 > score1){
-        printf("Player 1 wins!\n");
+        printf("Player 2 wins!\n");
     }else{
         printf("Tie!\n");
     }
