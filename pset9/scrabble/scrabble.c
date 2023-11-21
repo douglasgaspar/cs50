796dfddd48a7f6b5e4a7495d409848b6372eccfd
douglasgaspar <douglasgaspar@gmail.com>
2023-11-21 22:22:11 +0000

    Tue, Nov 21, 2023, 7:22 PM -03:00

--- pset9/scrabble/scrabble.c
+++ pset9/scrabble/scrabble.c
@@ -17,11 +17,16 @@ int main(void){
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
-    // TODO: Print the winner
+    printf("\n\nS1: %i", score1);
 }
 
 int compute_score(string word){
+    int sum = 0;
+
     for(int i=0 ; i<strlen(word) ; i++){
-        
+        int posicaoAscii = (int)tolower(word[i]) - 97;
+        printf(" %i ", POINTS[posicaoAscii]);
+        sum += POINTS[posicaoAscii];
     }
+    return sum;
 }
