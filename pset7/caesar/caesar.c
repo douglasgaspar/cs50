256fe4b29deffc3a61094a4d9cd70878eef81589
douglasgaspar <douglasgaspar@gmail.com>
2023-11-21 20:30:18 +0000

    Tue, Nov 21, 2023, 5:30 PM -03:00

--- pset7/caesar/caesar.c
+++ pset7/caesar/caesar.c
@@ -10,28 +10,30 @@ int main(int argc, string argv[]){
     if (argc == 2){ //Testa se tem o segundo argumento
         char char1 = argv[1][0];
 
-        //printf("%i\n", isdigit(char1));
-        if(isdigit(char1) != 0){ //Verificar se é número
-            string textoOriginal = get_string("plaintext: ");
-            int chave = atoi(argv[1]);
+        for(int i=0 ; i<strlen(argv[1]) ; i++){
+            if(isdigit(argv[1][i]) == 0){
+                printf("Usage: ./caesar key\n");
+                return 1;
+            }
+        }
 
-            printf("ciphertext: ");
+        string textoOriginal = get_string("plaintext: ");
+        int chave = atoi(argv[1]);
 
-            for(int i=0 ; i<(int)strlen(textoOriginal) ; i++){
-                if(isalpha(textoOriginal[i]) != 0){
-                    if(islower(textoOriginal[i])){
-                        printf("%c", (textoOriginal[i] - 97 + chave) % 26 + 97);
-                    }else if(isupper(textoOriginal[i])){
-                        printf("%c", (textoOriginal[i] - 65 + chave) % 26 + 65);
-                    }else{
-                        printf("%c", textoOriginal[i]);
-                    }
+        printf("ciphertext: ");
+
+        for(int i=0 ; i<(int)strlen(textoOriginal) ; i++){
+            if(isalpha(textoOriginal[i]) != 0){
+                if(islower(textoOriginal[i])){
+                    printf("%c", (textoOriginal[i] - 97 + chave) % 26 + 97);
+                }else if(isupper(textoOriginal[i])){
+                    printf("%c", (textoOriginal[i] - 65 + chave) % 26 + 65);
+                }else{
+                    printf("%c", textoOriginal[i]);
                 }
             }
-        }else{
-            printf("Usage: ./caesar key\n");
-            return 1;
         }
+
     }else{
         printf("Usage: ./caesar key\n");
         return 1;
